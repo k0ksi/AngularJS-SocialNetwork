59462637b2de6dbfaae7e39840ac59f8c8044217
Zhelyazko Stoyanov <zh.stoqnov@gmail.com>
2016-04-26 12:49:30 +0300

    removed hardcoded bearer token

--- app/users/identity.js
+++ app/users/identity.js
@@ -9,10 +9,7 @@ angular.module('socialNetwork.users.identity', [])
 
             var currentUser = undefined;
 
-            var accessToken = 'qatiyUtQqFLEvoxOLge0K-hJXCFlPIec6ZWQYDA269qvS0Tem9LZCV2X3_lTpwm5PJK66WmZJdHRDoSLRCfJzc9LrPIOdP_05UHvk1FIda-uCnPfF1nN3hvKIWzCoU2HRBZot4TRc0NxjxiBB2BfiQWLQutcGiAjNVLQxNYWcQ0A9owlT5Fu6uKKwPA-JwwaA5QxbxRmMOirlVIwL2L8tKoWwRtZTlS3R1qhHWvYCfJWTviG9f9dsv1zKKBrBE0QI8ezsQGk6WsyhtfBD8TjOHCwAagQII1Z1JdNBJXRwcl8jHL9j95kqHv6a7-OTmMi6qY1CNx4OLpqNLoxVQaETbEn5coNM_YEUoNCDDZLBLv1iFjIYfAXCvUy0qPieCFUPLkIIStRe_hpu7EQbAUsLKaHGUWCh7-cW4Nx66gPRImwyW308AJRM_Cb5L9j3s93KHBG2Os38JlptCqG6K6nHcznoSVkJhHhZrZMJa9cvDJlAzb19oF3i4f2kQT7mSmS';
-
-            $http.defaults.headers.common.Authorization =
-                'Bearer ' + accessToken;
+            // $http.defaults.headers.common.Authorization = 'Bearer ' + accessToken;
 
             $http.get(BASE_URL + 'me')
                 .then(function (response) {
